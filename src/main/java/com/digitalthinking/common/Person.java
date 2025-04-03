7c7fe926b9f1c84d17c9519687505108d115d57f
victorbadz <victorbadz@gmail.com>
2025-04-02 22:05:41 -0600

    carga inicial

---
+++ src/main/java/com/digitalthinking/common/Person.java
@@ -0,0 +1,74 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.digitalthinking.common;
+
+/**
+ *
+ * @author sotobotero
+ */
+/**
+ * This class contains a basic representation of entity person
+ * @author sotobotero
+ */
+public class Person {
+    
+    private long id;
+    private String name;
+    private String surname;
+    private String documentNumber;
+    private String phone;
+    private String address;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getDocumentNumber() {
+        return documentNumber;
+    }
+
+    public void setDocumentNumber(String documentNumber) {
+        this.documentNumber = documentNumber;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+    
+    
+}
